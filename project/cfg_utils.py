8632419ba6c109c2f64a8440576a8d985cdb62ad
Lyubov Miloserdova <55358539+miloserdova-l@users.noreply.github.com>
2021-10-22 08:07:42 +0100

    Task 6 (#5)

---
+++ project/cfg_utils.py
@@ -0,0 +1,21 @@
+from pyformlang.cfg import CFG, Production
+
+
+def cfg_to_normal_form(cfg: CFG) -> CFG:
+    is_gen_eps = cfg.generate_epsilon()
+    cfg = cfg.to_normal_form()
+
+    if is_gen_eps:
+        return CFG(
+            cfg.variables,
+            cfg.terminals,
+            cfg.start_symbol,
+            cfg.productions | {Production(cfg.start_symbol, [])},
+        )
+
+    return cfg
+
+
+def read_cfg_from_file(path: str, start_symbol: str = "S") -> CFG:
+    with open(path) as f:
+        return CFG.from_text(f.read(), start_symbol=start_symbol)
---
+++ tests/test_cfg_utils.py
@@ -0,0 +1,140 @@
+import os
+
+import pytest
+
+from pyformlang.cfg import CFG
+from pyformlang.cfg import Production, Variable, Terminal
+from project import cfg_to_normal_form, read_cfg_from_file
+
+root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+@pytest.mark.parametrize(
+    "cfg, gen_eps",
+    [
+        (
+            """
+    """,
+            False,
+        ),
+        # цепные
+        (
+            """
+        A -> B
+        B -> C
+        C -> A A
+        A -> epsilon
+    """,
+            False,
+        ),
+        # непорождающие
+        (
+            """
+        S -> a S
+        S -> epsilon
+        A -> a
+    """,
+            True,
+        ),
+        # длинные
+        (
+            """
+        S -> A S B
+        S -> epsilon
+        A -> a
+        B -> b
+    """,
+            True,
+        ),
+        # epsilon
+        (
+            """
+        S -> A S B
+        S -> epsilon
+        A -> a
+        B -> epsilon
+    """,
+            True,
+        ),
+    ],
+)
+def test_cfg_to_normal_form(cfg, gen_eps):
+    cnf = cfg_to_normal_form(CFG.from_text(cfg))
+    assert all(
+        production.is_normal_form() if production.body else True
+        for production in cnf.productions
+    )
+    assert cnf.generate_epsilon() == gen_eps
+
+
+@pytest.mark.parametrize(
+    "path, expected",
+    [
+        ("cfg-1.txt", set()),
+        ("cfg-2.txt", {Production(Variable("S"), [Terminal("a")])}),
+        (
+            "cfg-3.txt",
+            {
+                Production(Variable("A"), [Variable("B")]),
+                Production(Variable("B"), [Variable("C")]),
+                Production(Variable("C"), [Variable("A"), Variable("A")]),
+                Production(Variable("A"), []),
+                Production(Variable("A"), [Terminal("a")]),
+            },
+        ),
+    ],
+)
+def test_read_cfg_from_file(path, expected):
+    cfg = read_cfg_from_file(os.sep.join([root_path, "tests", "resources", path]))
+    actual = cfg.productions
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "cfg, accepted, rejected",
+    [
+        (
+            """
+        """,
+            [],
+            ["", "a"],
+        ),
+        (
+            """
+                S -> a S
+                S -> epsilon
+                A -> a
+            """,
+            ["", "a", "aa", "aaaaaaaaa"],
+            ["aS", "ab"],
+        ),
+        (
+            """
+                S -> A S B
+                S -> epsilon
+                A -> a
+                B -> b
+            """,
+            ["", "ab", "aabb", "aaaaabbbbb"],
+            ["abab", "aabbb", "aaabb", "ba"],
+        ),
+        (
+            """
+                S -> A S B
+                S -> epsilon
+                A -> a
+                B -> epsilon
+                B -> b
+            """,
+            ["", "ab", "aabb", "aaaaabbbbb", "aaaab", "aaabb", "aaa"],
+            ["ba", "abab", "aabbb"],
+        ),
+    ],
+)
+def test_equivalence(cfg, accepted, rejected):
+    cfg = CFG.from_text(cfg)
+    cnf = cfg_to_normal_form(cfg)
+    assert all(cnf.contains(w) and cfg.contains(w) for w in accepted) and all(
+        not cnf.contains(w) and not cfg.contains(w) for w in rejected
+    )
