896edf9d88d0944572bacca4f9a984a6186fd2e9
Lyubov Miloserdova <55358539+miloserdova-l@users.noreply.github.com>
2021-11-19 14:44:36 +0300

    Task 9 (#8)

---
+++ project/cfpq.py
@@ -0,0 +1,111 @@
+from collections import deque
+from typing import Tuple
+
+from networkx import MultiDiGraph
+from pyformlang.cfg import CFG, Variable
+from scipy.sparse import dok_matrix
+
+from project.cfg_utils import cfg_to_normal_form
+
+
+def cfpq(
+    graph: MultiDiGraph,
+    cfg: CFG,
+    start_nodes: set[int] = None,
+    final_nodes: set[int] = None,
+    start: Variable = Variable("S"),
+) -> set[Tuple[int, int]]:
+    cfg._start_symbol = start
+    if not start_nodes:
+        start_nodes = graph.nodes
+    if not final_nodes:
+        final_nodes = graph.nodes
+    result = set()
+    for u, h, v in hellings(cfg, graph):
+        if h == cfg.start_symbol.value and u in start_nodes and v in final_nodes:
+            result.add((u, v))
+    return result
+
+
+def hellings(cfg: CFG, graph: MultiDiGraph) -> set[Tuple[int, str, int]]:
+    n = graph.number_of_nodes()
+    if n == 0:
+        return set()
+
+    result = {}
+    deq = deque()
+    term_prods = set()
+    nonterm_prods = set()
+
+    if cfg.generate_epsilon():
+        matrix = dok_matrix((n, n), dtype=bool)
+        for i in range(n):
+            matrix[i, i] = True
+            deq.append((i, cfg.start_symbol.value, i))
+        result[cfg.start_symbol.value] = matrix
+
+    cfg = cfg_to_normal_form(cfg)
+
+    for prod in cfg.productions:
+        if len(prod.body) == 1:
+            term_prods.add(prod)
+        elif len(prod.body) == 2:
+            nonterm_prods.add(prod)
+
+    for u, v, edge_data in graph.edges(data=True):
+        for prod in term_prods:
+            if prod.body[0].value == edge_data["label"]:
+                deq.append((u, prod.head.value, v))
+                matrix = result.get(prod.head.value, dok_matrix((n, n), dtype=bool))
+                matrix[u, v] = True
+                result[prod.head.value] = matrix
+
+    while deq:
+        tmp = list()
+        out, var, to = deq.popleft()
+
+        for key, matrix in result.items():
+            for (i, _), _ in matrix.getcol(out).todok().items():
+                for prod in nonterm_prods:
+                    if (
+                        prod.body[0].value == key
+                        and prod.body[1].value == var
+                        and (
+                            prod.head.value not in result
+                            or result[prod.head.value][i, to] == False
+                        )
+                    ):
+                        deq.append((i, prod.head.value, to))
+                        tmp.append((i, prod.head.value, to))
+
+        for out, var, to in tmp:
+            matrix = result.get(var, dok_matrix((n, n), bool))
+            matrix[out, to] = True
+            result[var] = matrix
+        tmp.clear()
+
+        for key, matrix in result.items():
+            for (_, new_to), _ in matrix.getrow(to).todok().items():
+                for prod in nonterm_prods:
+                    if (
+                        prod.body[0].value == var
+                        and prod.body[1].value == key
+                        and (
+                            prod.head.value not in result
+                            or result[prod.head.value][out, new_to] == False
+                        )
+                    ):
+                        deq.append((out, prod.head.value, new_to))
+                        tmp.append((out, prod.head.value, new_to))
+
+        for out, var, to in tmp:
+            matrix = result.get(var, dok_matrix((n, n), bool))
+            matrix[out, to] = True
+            result[var] = matrix
+
+    triples = set()
+    for key, matrix in result.items():
+        for (u, v), _ in matrix.items():
+            triples.add((u, key, v))
+
+    return triples
---
+++ tests/test_cfpq.py
@@ -0,0 +1,79 @@
+import pytest
+from cfpq_data import labeled_cycle_graph
+from pyformlang.cfg import CFG
+
+from project import create_labeled_two_cycles_graph
+from project.cfpq import cfpq
+
+
+@pytest.mark.parametrize(
+    "cfg, graph, start_nodes, final_nodes, exp_ans",
+    [
+        (
+            """
+            S -> epsilon
+            """,
+            labeled_cycle_graph(5, "a"),
+            None,
+            None,
+            {(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)},
+        ),
+        (
+            """
+                S -> a | epsilon
+                """,
+            labeled_cycle_graph(3, "a"),
+            {0},
+            {0, 1},
+            {
+                (0, 0),
+                (0, 1),
+            },
+        ),
+        (
+            """
+                S -> A B
+                S -> A C
+                C -> S B
+                A -> a
+                B -> b
+            """,
+            create_labeled_two_cycles_graph(3, 2, ("a", "b")),
+            None,
+            None,
+            {
+                (3, 4),
+                (2, 5),
+                (1, 0),
+                (0, 4),
+                (3, 5),
+                (2, 0),
+                (1, 4),
+                (0, 5),
+                (3, 0),
+                (2, 4),
+                (1, 5),
+                (0, 0),
+            },
+        ),
+        (
+            """
+                S -> A B
+                S -> A C
+                C -> S B
+                A -> a
+                B -> b
+            """,
+            create_labeled_two_cycles_graph(3, 2, ("a", "b")),
+            {0},
+            None,
+            {
+                (0, 4),
+                (0, 5),
+                (0, 0),
+            },
+        ),
+    ],
+)
+def test_cfpq(cfg, graph, start_nodes, final_nodes, exp_ans):
+    assert cfpq(graph, CFG.from_text(cfg), start_nodes, final_nodes) == exp_ans
